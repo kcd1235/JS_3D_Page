daa7d7f2343d6cfb00e983fcac904d38b79f2aa5
kcd <kcd1235@naver.com>
2023-05-21 22:09:31 +0900

    이미지수정

--- JS/main.js
+++ JS/main.js
@@ -1,75 +1,65 @@
-/*================= 변수선언 ==================== */
+// 변수 선언부
+var wrapper = document.querySelector(".wrapper"),
+  page = document.querySelectorAll(".page"),
+  indicator = document.getElementById("indicator"),
+  indicator_li = indicator.querySelectorAll("li");
 
-var wrapper = document.querySelector(".wrapper"); //전체 페이지를 감싸는 틀
-var page = document.querySelectorAll(".page"); //각 페이지 요소
-var indicator = document.getElementById("indicator"); //인디케이터를 담는 틀
-var indicator_li = indicator.querySelectorAll("li"); //인디케이터 목록
+var yDeg = 0,
+  indicator_num = 1,
+  indicator_length = page.length,
+  w = page[0].offsetWidth,
+  page_angle = 0,
+  page_vector = 0;
 
-var yDeg = 0;
-var indicator_num = 1;
-var indicator_length = page.length;
-var w = page[0].offsetWidth;
-var page_angle = 0;
+var hammer = new Hammer(wrapper);
 
-var hammer = new Hammer(wrapper); //터치 객체 생성
-
-/*================= 페이지 초기화 ==================== */
-
+// 페이지 초기화
 function init_page() {
   w = page[0].offsetWidth;
 
-  //3D페이지 4면체 위치 정의
-  /*================= 3D페이지 4면체 위치 정의 ==================== */
+  // 3D page 4면체 위치 정의
   for (var i = 0; i < page.length; i++) {
-    page[i].style_transform =
+    page[i].style.transform =
       "rotateY(" + page_angle + "deg) translateZ(" + w / 2 + "px)";
     page_angle += 90;
   }
 
-  /*================= 페이지 정면으로 초기화 ==================== */
+  // page wrapper 정면으로 초기화
   wrapper.style.transform =
     "translateZ(" + -w / 2 + "px) rotateY(" + yDeg + "deg)";
 }
 
-/*================= 페이지 초기화 끝 ==================== */
-
-/*================= 인디케이터 초기화 함수 ==================== */
-
+// 인디케이터 초기화
 function init_indicator() {
-  /*================= 인디케이터 표시 ==================== */
+  // 인디케이터 표시
   for (var i = 0; i < indicator_length; i++) {
     indicator.innerHTML += "<li>" + (i + 1) + "</li>";
   }
 
-  indicator_li = indicator.querySelectorAll("li");
+  indicator_li = indicator.querySelectorAll("li"); // 목록
   change_page(indicator_num);
 }
 
-/*================= 인디케이터 초기화 끝 ==================== */
-
-/*================= 페이지 전환 ==================== */
-
+// 페이지 전환
 function change_page(inum) {
+  // 현재 인디케이터 하이라이트 표시
   indicator_li[inum - 1].setAttribute("class", "active");
   yDeg = -90 * (inum - 1);
   wrapper.style.transform =
     "translateZ(" + -w / 2 + "px) rotateY(" + yDeg + "deg)";
 
-  /*================= 인디케이터 표시 ==================== */
+  // 인디케이터 표시
   for (var i = 0; i < indicator_li.length; i++) {
     indicator_li[i].removeAttribute("class");
   }
-
   indicator_li[inum - 1].setAttribute("class", "active");
 }
 
-/*================= 페이지 전환 끝 ==================== */
-
+/* ---------------------------------------------------------------- */
 init_page();
 init_indicator();
 
-/*================= 이벤트리스너 ==================== */
-
+/* ------------------- 이벤트 리스너 ------------------------------ */
 for (var i = 0; i < indicator_li.length; i++) {
   indicator_li[i].addEventListener("click", function () {
     indicator_num = parseInt(this.innerText);
@@ -77,13 +67,9 @@ for (var i = 0; i < indicator_li.length; i++) {
   });
 }
 
-/*================= 이벤트리스너 끝 ==================== */
-
-/*================= 터치 제스처 이벤트 ==================== */
-
 // 터치 swipe left
 hammer.on("swipeleft", function (e) {
-  //인디케이터(페이지) 이동 범위 내이면
+  // 인디케이터(페이지) 이동 범위 내이면
   if (indicator_num < indicator_length) {
     page_vector = 1;
   } else page_vector = 0;
@@ -92,7 +78,7 @@ hammer.on("swipeleft", function (e) {
   change_page(indicator_num);
 });
 
-//터치 swipe right
+// 터치 swipe right
 hammer.on("swiperight", function (e) {
   if (indicator_num > 1) {
     page_vector = -1;
@@ -102,9 +88,7 @@ hammer.on("swiperight", function (e) {
   change_page(indicator_num);
 });
 
-//창 크기 변경 시 페이지 초기화
+// 창크기 변경시 페이지 초기화
 window.onresize = function () {
   init_page();
 };
-
-/*================= 터치 제스처 이벤트 끝 ==================== */
