a21873d279b54656a5df5e17c5b35bc84f9af066
kcd <kcd1235@naver.com>
2023-05-21 19:39:48 +0900

    이벤트 리스너 구현

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
-// 변수선언
+/*================= 변수선언 ==================== */
+
 var wrapper = document.querySelector(".wrapper"); //전체 페이지를 감싸는 틀
 var page = document.querySelectorAll(".page"); //각 페이지 요소
 var indicator = document.getElementById("indicator"); //인디케이터를 담는 틀
@@ -10,26 +11,30 @@ var indicator_length = page.length;
 var w = page[0].offsetWidth;
 var page_angle = 0;
 
-//페이지 초기화
+/*================= 페이지 초기화 ==================== */
+
 function init_page() {
   w = page[0].offsetWidth;
 
   //3D페이지 4면체 위치 정의
+  /*================= 3D페이지 4면체 위치 정의 ==================== */
   for (var i = 0; i < page.length; i++) {
     page[i].style_transform =
       "rotateY(" + page_angle + "deg) translateZ(" + w / 2 + "px)";
     page_angle += 90;
   }
 
-  //페이지 정면으로 초기화
+  /*================= 페이지 정면으로 초기화 ==================== */
   wrapper.style.transform =
     "translateZ(" + -w / 2 + "px) rotateY(" + yDeg + "deg)";
 }
-//페이지 초기화 끝
 
-//인디케이터 초기화 함수
+/*================= 페이지 초기화 끝 ==================== */
+
+/*================= 인디케이터 초기화 함수 ==================== */
+
 function init_indicator() {
-  //인디케이터 표시
+  /*================= 인디케이터 표시 ==================== */
   for (var i = 0; i < indicator_length; i++) {
     indicator.innerHTML += "<li>" + (i + 1) + "</li>";
   }
@@ -37,16 +42,18 @@ function init_indicator() {
   indicator_li = indicator.querySelectorAll("li");
   change_page(indicator_num);
 }
-//인디케이터 초기화 끝
 
-//페이지 전환
+/*================= 인디케이터 초기화 끝 ==================== */
+
+/*================= 페이지 전환 ==================== */
+
 function change_page(inum) {
   indicator_li[inum - 1].setAttribute("class", "active");
   yDeg = -90 * (inum - 1);
   wrapper.style.transform =
     "translateZ(" + -w / 2 + "px) rotateY(" + yDeg + "deg)";
 
-  //인디케이터 표시
+  /*================= 인디케이터 표시 ==================== */
   for (var i = 0; i < indicator_li.length; i++) {
     indicator_li[i].removeAttribute("class");
   }
@@ -54,5 +61,18 @@ function change_page(inum) {
   indicator_li[inum - 1].setAttribute("class", "active");
 }
 
+/*================= 페이지 전환 끝 ==================== */
+
 init_page();
 init_indicator();
+
+/*================= 이벤트리스너 ==================== */
+
+for (var i = 0; i < indicator_li.length; i++) {
+  indicator_li[i].addEventListener("click", function () {
+    indicator_num = parseInt(this.innerText);
+    change_page(indicator_num);
+  });
+}
+
+/*================= 이벤트리스너 끝 ==================== */
