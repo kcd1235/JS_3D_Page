7e381950f28d9b9a266389a3cf00782f2d528d7e
kcd <kcd1235@naver.com>
2023-05-21 19:28:05 +0900

    페이지 초기화

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,21 @@ var indicator_num = 1;
 var indicator_length = page.length;
 var w = page[0].offsetWidth;
 var page_angle = 0;
+
+//페이지 초기화
+function init_page() {
+  w = page[0].offsetWidth;
+
+  //3D페이지 4면체 위치 정의
+  for (var i = 0; i < page.length; i++) {
+    page[i].style_transform =
+      "rotateY(" + page_angle + "deg) translateZ(" + w / 2 + "px)";
+    page_angle += 90;
+  }
+
+  //페이지 정면으로 초기화
+  wrapper.style.transform =
+    "translateZ(" + -w / 2 + "px) rotateY(" + yDeg + "deg)";
+}
+
+init_page();
